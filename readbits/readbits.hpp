57bad7eef904cd06c42682e455c628c3c0287e0b
Ashwin Abraham <210050023@iitb.ac.in>
2022-08-01 19:52:02 +0530

    added header guards and namespace

--- readbits/readbits.hpp
+++ readbits/readbits.hpp
@@ -10,22 +10,30 @@ To know how the 3 would be stored as a float/double, call bigendian<float>(3) an
 
 Also if the object contains pointers this will not show the bits in the memory pointed to by these pointers*/
 
+#ifndef _READBITS_H_
+#define _READBITS_H_
+
 #include <vector>
 #define usize (8*sizeof(unsigned int))
 
-template <typename T>
-std::vector<bool> littleendian(const T& a)
-{
-    std::vector<bool> bits;
-    const unsigned int* ptra = reinterpret_cast<const unsigned int*>(&a);
-    for(int i = 0; i<8*sizeof(T); ++i) bits.push_back((ptra[i/usize]>>(i%usize))&1);
-    return bits;
-}
+namespace readbits{
+    template <typename T>
+    std::vector<bool> littleendian(const T& a)
+    {
+        std::vector<bool> bits;
+        const unsigned int* ptra = reinterpret_cast<const unsigned int*>(&a);
+        for(int i = 0; i<8*sizeof(T); ++i) bits.push_back((ptra[i/usize]>>(i%usize))&1);
+        return bits;
+    }
 
-template <typename T>
-std::vector<bool> bigendian(const T& a){
-    std::vector<bool> bits;
-    const unsigned int* ptra = reinterpret_cast<const unsigned int*>(&a);
-    for(int i = 8*sizeof(T)-1; i>=0; --i) bits.push_back((ptra[i/usize]>>(i%usize))&1);
-    return bits;
-}
+    template <typename T>
+    std::vector<bool> bigendian(const T& a){
+        std::vector<bool> bits;
+        const unsigned int* ptra = reinterpret_cast<const unsigned int*>(&a);
+        for(int i = 8*sizeof(T)-1; i>=0; --i) bits.push_back((ptra[i/usize]>>(i%usize))&1);
+        return bits;
+    }
+};
+
+#undef usize
+#endif
